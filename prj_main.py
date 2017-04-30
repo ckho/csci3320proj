c1c71cffcd68b11fc6c8d41c50e04518693ac002
Dicky Ho <me@ckho.info>
2017-04-30 22:43:50 +0800

    Minor Update

--- prj_main.py
+++ prj_main.py
@@ -1,9 +1,11 @@
 from preprocess import transform
+from preprocess import transform_for_test
 from preprocess import fill_missing
 
 from lr import LogisticRegression
 from naive_bayes import NaiveBayes
 
+from sklearn.feature_extraction import DictVectorizer as DV
 from sklearn import svm
 from sklearn import ensemble
 from sklearn.model_selection import train_test_split
@@ -86,33 +88,54 @@ def main():
   # test_dataset = transform_for_test(filename_test)
   # X_test = test_dataset['data']
 
-  # # TO DO: Fill Missing
+  # X_test = X_test.replace({False:-1, True:1})
+
+  # X_test.loc[X_test.YOB < 1920, 'YOB'] = 0
+  # X_test.loc[X_test.YOB > 2004, 'YOB'] = 0
+  # X_test.loc[X_test.YOB.isnull(), 'YOB'] = 0
+
+  # numeric_cols = ['YOB', 'votes']
+
+  # x_test_num = X_test[numeric_cols].as_matrix()
+  # x_test_num = x_test_num / x_max
+
+  # cat_X_test = X_test.drop(numeric_cols + ['UserID'], axis = 1)
+  # cat_X_test.fillna(0, inplace = True)
+  # x_test_cat = cat_X_test.T.to_dict().values()
+
+  # # vectorize
+  # vec_x_test_cat = vectorizer.transform(x_test_cat)
+
+  # # combine x
+  # x_test_completed = np.hstack((x_test_num, vec_x_test_cat))
+
+  # # # TO DO: Fill Missing
 
   # predictions_path = './predictions/'
 
   # ## do predictions
-  # lr_predictions = lr_model.predict(X_test)
+  # lr_predictions = lr_model.predict(x_test_completed)
   # lr_predictions_file = open(predictions_path + 'lr_predictions.csv', 'w')
   # print('UserID,Happy', file=lr_predictions_file)
-  # for x_test, prediction in zip(X_test, lr_predictions):
+  # for x_test, prediction in zip(x_test_completed, lr_predictions):
   #   print(str(int(x_test['UserID']))+','+str(int(prediction)), file=lr_predictions_file)
 
-  # nb_predictions = nb_model.predict(X_test)
+  # nb_predictions = nb_model.predict(x_test_completed)
   # nb_predictions_file = open(predictions_path + 'nb_predictions.csv', 'w')
   # print('UserID,Happy', file=nb_predictions_file)
-  # for x_test, prediction in zip(X_test, nb_predictions):
+  # for x_test, prediction in zip(x_test_completed, nb_predictions):
   #   print(str(int(x_test['UserID']))+','+str(int(prediction)), file=nb_predictions_file)
 
-  # svm_predictions = svm_model.predict(X_test)
+  # svm_predictions = svm_model.predict(x_test_completed)
   # svm_predictions_file = open(predictions_path + 'svm_predictions.csv', 'w')
   # print('UserID,Happy', file=svm_predictions_file)
-  # for x_test, prediction in zip(X_test, svm_predictions):
+  # for x_test, prediction in zip(x_test_completed, svm_predictions):
   #   print(str(int(x_test['UserID']))+','+str(int(prediction)), file=svm_predictions_file)
 
-  # rf_predictions = rf_model.predict(X_test)
+  # rf_predictions = rf_model.predict(x_test_completed)
   # rf_predictions_file = open(predictions_path + 'rf_predictions.csv', 'w')
   # print('UserID,Happy', file=rf_predictions_file)
-  # for x_test, prediction in zip(X_test, rf_predictions):
+  # for x_test, prediction in zip(x_test_completed, rf_predictions):
   #   print(str(int(x_test['UserID']))+','+str(int(prediction)), file=rf_predictions_file)
 
 
